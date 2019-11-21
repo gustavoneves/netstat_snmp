0c16e5545aa64c770adeee89a7698e6caa11f028
gustavo neves <gustavo.neves@yandex.com>
2019-11-21 12:42:42 -0300

    correcoes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ var app = express();
 
 var snmp = require('net-snmp');
 
+var h = "";
+
 app.use(express.static('public'));
 
 
@@ -23,13 +25,15 @@ app.get('/process_get', function (req, res) {
     // snmpwalk -v 1 -c read_only_community_string localhost
     var snmpSessao = snmp.createSession(resposta.ip, resposta.community);
 
+    var teste2;
+
     function doneCbUDP (error) {
         if (error){
             console.error (error.toString ());
         }
         else{
-            //console.log("TESTE: " + teste);
-            return res.send("Portas udp abertas: " + teste);
+            teste = teste + "</table> <p><input type=\"button\" value=\"Nova Busca\" onclick=\"history.back()\"></input></p></body> </html>";
+            return res.send(teste);
         }
     }
 
@@ -38,51 +42,105 @@ app.get('/process_get', function (req, res) {
             console.error (error.toString ());
         }
         else{
-            //console.log("TESTE: " + teste);
-            return res.send("Portas tcp ConnState: " + teste);
+            teste = teste + "</table> <p><input type=\"button\" value=\"Nova Busca\" onclick=\"history.back()\"></input></p></body> </html>";
+            return res.send(teste);
+        }
+    }
+
+    function doneCbUDP2 (error) {
+        if (error){
+            console.error (error.toString ());
+        }
+        else{
+           var t2 = "";
+           var oidTCPConn = "1.3.6.1.2.1.6.13.1.1";
+           teste2 = teste + "</table> <br> </br> <table> <tr><th>Endereços/Portas Locais</th><th>Endereços/Portas Remotas</th></tr>"
+
+           snmpSessao.subtree(oidTCPConn, function (varbinds) {
+
+                for (var i = 0; i < varbinds.length; i++) {
+                    if(varbinds[i].value === 5){ // 5 e' established
+                        t2 = (varbinds[i].oid);
+                        t2 = t2.substring(oidTCPConn.length + 1); // + 1 devido ao ponto
+                        t2 = "<tr><td>" + t2;
+                    
+                                
+                        var n2 = 1;
+                        t2 = t2.replace(/\./g, v => n2++ == 5 ? "</td><td>" : v); // remove o quinto ponto
+                        n2=1;
+                        t2 = t2.replace(/\./g, v => n2++ == 4 ? ":" : v); // troca o quarto ponto
+                        n2=1;
+                        t2 = t2.replace(/\./g, v => n2++ == 7 ? ":" : v); // troca o setimo ponto
+                        t2 = t2 + "</td>";
+
+                        teste2 = teste2 + t2;
+                    }
+                }
+            }, doneCbTCPcon2);
+        }
+    }
+
+    function doneCbTCPcon2 (error) {
+        if (error){
+            console.error (error.toString ());
+        }
+        else{
+            teste2 = teste2 + "</table> <p><input type=\"button\" value=\"Nova Busca\" onclick=\"history.back()\"></input></p></body> </html>";
+            return res.send(teste2);
         }
     }
  
     if(resposta.botao){
+
         if(resposta.pTCP && resposta.pUDP){
-            return res.send("ip: " + resposta.ip + " " + "TCP" + " e " + "UDP");
+           
+           var teste = "<html> <head> <style> table, th, td { border: 1px solid black; border-collapse: collapse;} </style> </head><body> <table> <tr><th>Portas UDP</th></tr>"
+           var t = "";
+           const oidsUDP = "1.3.6.1.2.1.7.5.1.2";
+           snmpSessao.subtree(oidsUDP, function (varbinds) {
+                   for (var i = 0; i < varbinds.length; i++) {
+                       t = varbinds[i].value;
+                       teste = teste.concat("<tr><td>" + t + "</td></tr>");
+                   }            
+           }, doneCbUDP2);
         }
         else{
             if(resposta.pUDP){
-                var teste = "<br>";
+                var teste = "<html> <head> <style> table, th, td { border: 1px solid black; border-collapse: collapse;} </style> </head><body> <table> <tr><th>Portas UDP</th></tr>"
                 var t = "";
                 const oidsUDP = "1.3.6.1.2.1.7.5.1.2";
                 snmpSessao.subtree(oidsUDP, function (varbinds) {
                         for (var i = 0; i < varbinds.length; i++) {
-                            //console.log (varbinds[i].oid + "|" + varbinds[i].value);
                             t = varbinds[i].value;
                        }
-                       teste = teste.concat(t + "<br>");
+                       teste = teste.concat("<tr><td>" + t + "</td></tr>");
                 }, doneCbUDP);
             }
             else{
-                var teste = "<br>";
+                var teste = "<html> <head> <style> table, th, td { border: 1px solid black; border-collapse: collapse;} </style> </head><body> <table> <tr><th>Endereços/Portas Locais</th><th>Endereços/Portas Remotas</th></tr>"
+
                 var t = "";
                 var oidTCPConn = "1.3.6.1.2.1.6.13.1.1";
                 snmpSessao.subtree(oidTCPConn, function (varbinds) {
                         for (var i = 0; i < varbinds.length; i++) {
-                            //console.log (varbinds[i].oid + "|" + varbinds[i].value);
-                            //t = varbinds[i].value;
                             if(varbinds[i].value === 5){ // 5 e' established
                                 t = (varbinds[i].oid);
                                 t = t.substring(oidTCPConn.length + 1); // + 1 devido ao ponto
+                                t = "<tr><td>" + t;
+                                console.log(t);
+                                
                                 var n = 1;
-                                t = t.replace(/\./g, v => n++ == 5 ? " " : v); // remove o quinto ponto
+                                t = t.replace(/\./g, v => n++ == 5 ? "</td><td>" : v); // remove o quinto ponto
                                 n=1;
                                 t = t.replace(/\./g, v => n++ == 4 ? ":" : v); // troca o quarto ponto
                                 n=1;
                                 t = t.replace(/\./g, v => n++ == 7 ? ":" : v); // troca o setimo ponto
+                                t = t + "</td>";
+
+                                teste = teste + t;
                             }
                        }
-                       teste = teste.concat(t + "<br>");
                 }, doneCbTCPcon);
-
-                //return res.send("ip: " + resposta.ip + " " + "TCP");
             }
         }     
     }
